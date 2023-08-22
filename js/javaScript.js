c7640e9fe074df81de601edb18ca68486e8284a9
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2023-08-21 18:15:32 -0600

    first commit

---
+++ js/javaScript.js
@@ -0,0 +1,6 @@
+let cadenaAConvertir = "HOLA MUNDO";
+//convertiremos el texto de mayusculas a minusculas
+console.log("Mostramos el texto con mayusculas : " , cadenaAConvertir);
+
+console.log("Mostramos el texto con ayuda de tolowercase en convertirlo a minusculas : " , cadenaAConvertir.toLowerCase());
+//ahora iremos a nuestro navegador a ver el resultado
